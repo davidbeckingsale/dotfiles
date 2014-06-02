2a59bb1508cd145ec16f9f1f3519fc7918956866
David Beckingsale <davidbeckingsale@gmail.com>
2014-06-01 18:57:23 -0700

    Fix space error in oh-my-zsh/install

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d ~/.oh-my-zsh]; then
+if [ ! -d ~/.oh-my-zsh ]; then
   echo "  Installing oh-my-zsh for you."
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
