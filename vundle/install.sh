22ab30b4c5933eba2797e6f929bdeea49a5ae6df
David Beckingsale <davidbeckingsale@gmail.com>
2015-07-30 12:16:50 -0700

    Misc updates

--- vundle/install.sh
+++ vundle/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 if [ ! -d ~/.vim/bundle/vundle ]; then
-  git clone git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/vundle
-  vim +PluginInstall +qall
+  /bin/mkdir -p ~/.vim/bundle/vundle
 fi
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+vim +PluginInstall +qall
